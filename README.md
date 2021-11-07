2a5df7ceba14be43ed0dd78c0503b1d9c33dd885
Manoj Kumar Maurya <manojk.maurya@gmail.com>
2021-11-07 21:40:13 +0530

    new change

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 Run:
 docker-compose up --scale node=5
+
+Connect to image:
+docker run --rm -it --entrypoint=/bin/sh node
+
--- get_name.sh
+++ get_name.sh
@@ -1,4 +1,5 @@
 DOCKERINFO=$(curl -s --unix-socket /run/docker.sock http://docker/containers/$HOSTNAME/json)
 ID=$(python3 -c "import sys, json; print(json.loads(sys.argv[1])[\"Name\"].split(\"_\")[-1])" "$DOCKERINFO")
+echo "This is node info $DOCKERINFO"
 echo "This is node $ID"
 
